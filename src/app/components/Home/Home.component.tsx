687e4cba4b5b046c729898402b95c89d2dee466e
Vladimir Yelisavetski <vladimiry@spectory.com>
2021-01-15 19:34:05 +0200

    first commit

--- src/app/components/Home/Home.component.tsx
+++ src/app/components/Home/Home.component.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Content = styled.div`
+const Page = styled.div`
+	display: flex;
+	flex-direction: column;
 	background-color: salmon;
 `;
 
+const MenuButton = styled.div`
+	height: 40px;
+	width: 150px;
+	border: 1px solid black;
+	background-color: pink;
+	cursor: pointer;
+`;
+
 const Home: React.FunctionComponent = () => {
-	return <Content>home page2</Content>;
+	const handleGetUserData = () => {
+		console.log('handleGetUserData');
+	};
+
+	const handleGetAllPets = () => {
+		console.log('handleGetAllPets');
+	};
+
+	const handleGetUsers = () => {
+		console.log('handleGetUsers');
+	};
+
+	return (
+		<Page>
+			Home component
+			<MenuButton onClick={handleGetAllPets}>Get all pets</MenuButton>
+			<MenuButton onClick={handleGetUsers}>Get all users</MenuButton>
+			<MenuButton onClick={handleGetUserData}>Get user data</MenuButton>
+		</Page>
+	);
 };
 
 export default Home;
